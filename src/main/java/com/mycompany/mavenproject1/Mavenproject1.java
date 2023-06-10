89d2a5572abac797ec7638ae3390bb0b350ddf14
Tatiana <TSHurygina@IBS.RU>
2023-06-10 09:32:32 +0300

    задача 2: типы данных

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,12 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
+        /*задание 1*/
         System.out.println("Hello World!");
+         /*задание 2*/
+         double a = 4.145;
+         byte b = 127;
+         System.out.println(a);
+         System.out.println(b);
     }
 }

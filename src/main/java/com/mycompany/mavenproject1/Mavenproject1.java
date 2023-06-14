15de2921c760d29f37f7c8361f24d402ea6e3042
Tatiana <TSHurygina@IBS.RU>
2023-06-14 18:32:20 +0300

    задача 3: массивы

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -16,7 +16,12 @@ public class Mavenproject1 {
          /*задание 2*/
          double a = 4.145;
          byte b = 127;
-         System.out.println(a);
-         System.out.println(b);
+         /*задание 3*/
+         int[] nums = {1,2,3,4,5};
+         int var = nums[0];
+         nums[0]=nums[nums.length-1];
+         nums[nums.length-1] = var;
+         int sum = nums[0] + nums[nums.length/2];
+         System.out.println(sum);
     }
 }

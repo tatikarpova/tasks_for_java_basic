f562cf0bb027a3101def20f04d0a1ec531bf9767
Tatiana <TSHurygina@IBS.RU>
2023-06-16 19:20:25 +0300

    задача 4: Операторы

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.mavenproject1;
+import java.util.Scanner;
 
 /**
  *
@@ -23,5 +24,20 @@ public class Mavenproject1 {
          nums[nums.length-1] = var;
          int sum = nums[0] + nums[nums.length/2];
          System.out.println(sum);
+         
+         /*задание4*/
+         Scanner in = new Scanner(System.in);
+         System.out.println("Введите число 1: ");
+         int x = in.nextInt();
+         System.out.println("Введите число 2: ");
+         int y = in.nextInt();
+         System.out.println("Введите число 3: ");
+         int z = in.nextInt();
+         int num = (x+y+z)/3;
+         System.out.printf("Среднее арифметическое чисел = %s%n", num);
+         int num2 = num/2;
+         if (num2 > 3) System.out.println("Программа выполнена корректно");
+             
+         
     }
 }
